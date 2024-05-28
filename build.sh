6b74689adc473d5289bb4037b4c584ca8b8dafe8
vishalbansal28 <bornconfusde@gmail.com>
2024-05-28 11:58:53 +0530

    first commit

---
+++ build.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+# Variables
+cluster_name="cluster-1-test"
+region="REGION"
+aws_id="AWS_ID"
+cd terraform 
+app_img=$(terraform output -raw ecr_app_repository_name) 
+db_img=$(terraform output -raw ecr_db_repository_name)
+rds_endpoint=$(terraform output -raw rds_cluster_endpoint)
+db_username=$(terraform output -raw db_username)
+db_password=$(terraform output -raw db_password)
+app_image_name="$aws_id.dkr.ecr.$region.amazonaws.com/$app_img:latest"
+db_image_name="$aws_id.dkr.ecr.$region.amazonaws.com/$db_img:latest"
+cd ..
+namespace="todo-app"
+monitoring_ns="monitoring"
+app_service_name="todo-app-service"
+alertmanager_service_name="kube-prometheus-stack-alertmanager"
+prometheus_service_name="kube-prometheus-stack-prometheus"
+grafana_service_name="kube-prometheus-stack-grafana"
+# End Variables
+
+# update helm repos
+helm repo update
+
+# create the cluster
+# echo "--------------------Creating EKS--------------------"
+# echo "--------------------Creating ECR--------------------"
+# echo "--------------------Creating EBS--------------------"
+# echo "--------------------Creating RDS--------------------"
+# echo "--------------------Deploying Monitoring--------------------"
+cd terraform && \
+terraform init 
+terraform apply -auto-approve
+cd ..
+
+# update kubeconfig
+echo "--------------------Update Kubeconfig--------------------"
+aws eks update-kubeconfig --name $cluster_name --region $region
+
+# remove preious docker images
+echo "--------------------Remove Previous build--------------------"
+docker rmi -f $app_image_name || true
+docker rmi -f $db_image_name || true
+
+# build new docker image with new tag
+echo "--------------------Build new Image--------------------"
+docker build -t $app_image_name todo-app/
+docker build -f k8s/Dockerfile.mysql -t $db_image_name k8s
+
+# ECR Login
+echo "--------------------Login to ECR--------------------"
+aws ecr get-login-password --region $region | docker login --username AWS --password-stdin $aws_id.dkr.ecr.eu-central-1.amazonaws.com
+
+# push the latest build to dockerhub
+echo "--------------------Pushing Docker Image--------------------"
+docker push $app_image_name
+docker push $db_image_name
+
+# create namespace
+echo "--------------------creating Namespace--------------------"
+kubectl create ns $namespace || true
+
+# add rds endpoint into k8s secrets
+echo "--------------------Create RDS Secrets --------------------"
+kubectl create secret -n $namespace generic rds-endpoint --from-literal=endpoint=$rds_endpoint || true
+kubectl create secret -n $namespace generic rds-username --from-literal=username=$db_username || true
+kubectl create secret -n $namespace generic rds-password --from-literal=password=$db_password || true
+
+# deploy the application
+echo "--------------------Deploy App--------------------"
+kubectl apply -n $namespace -f k8s/
+
+# Wait for application to be deployed
+echo "--------------------Wait for all pods to be running--------------------"
+sleep 60s
+
+# Get ingress URL
+echo "--------------------Application LoadBalancer URL--------------------"
+kubectl get svc -n ${namespace} ${app_service_name} -o=custom-columns=EXTERNAL-IP:.status.loadBalancer.ingress[*].hostname | tail -n +2
+echo "-------------------- Alertmanager LoadBalancer URL--------------------"
+kubectl get svc -n ${monitoring_ns} ${alertmanager_service_name} -o=custom-columns=EXTERNAL-IP:.status.loadBalancer.ingress[*].hostname | tail -n +2
+echo "--------------------Prometheus LoadBalancer URL--------------------"
+kubectl get svc -n ${monitoring_ns} ${prometheus_service_name} -o=custom-columns=EXTERNAL-IP:.status.loadBalancer.ingress[*].hostname | tail -n +2
+echo "--------------------Grafana LoadBalancer URL--------------------"
+kubectl get svc -n ${monitoring_ns} ${grafana_service_name} -o=custom-columns=EXTERNAL-IP:.status.loadBalancer.ingress[*].hostname | tail -n +2
+
+# Get RDS endpoint URL
+echo "--------------------RDS endpoint URL--------------------"
+echo $rds_endpoint
---
+++ destroy.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Variables
+cluster_name="cluster-1-test"
+region="REGION"
+aws_id="AWS_ID"
+cd terraform 
+app_img=$(terraform output -raw ecr_app_repository_name) 
+db_img=$(terraform output -raw ecr_db_repository_name)
+elastic_ip_name=$(terraform output -raw 
+app_image_name="$aws_id.dkr.ecr.eu-central-1.amazonaws.com/$app_img:latest"
+db_image_name="$aws_id.dkr.ecr.eu-central-1.amazonaws.com/$db_repo:latest"
+rds_snapshot_name=$(terraform output -raw final_snapshot_name)
+cd ..
+namespace="todo-app"
+# End Variables
+
+# remove preious docker images
+echo "--------------------Remove Previous build--------------------"
+docker rmi -f $app_image_name || true
+docker rmi -f $db_image_name || true
+
+# delete Docker-img from ECR
+echo "--------------------Deleting ECR-IMG--------------------"
+./ecr-imgs-delete.sh $app_img $region 
+./ecr-imgs-delete.sh $db_img $region 
+
+
+# Release Elastic IP
+echo "--------------------Releasing Elastic IP--------------------"
+allocation_id=$(aws ec2 describe-addresses --region $region --filters "Name=tag:Name,Values=${cluster_name}-nat-eip" --query 'Addresses[].AllocationId' --output text)
+if [ ! -z "$allocation_id" ]; then
+    aws ec2 release-address --allocation-id $allocation_id --region $region
+fi
+
+# Destroy Infrastructure
+# echo "--------------------Destroy Infrastructure--------------------"
+cd terraform && \ 
+terraform destroy -auto-approve
+
+# Delete rds snapshot
+echo "--------------------Delete Rds Snapshot--------------------"
+aws rds delete-db-cluster-snapshot --db-cluster-snapshot-identifier $rds_snapshot_name --region $region
+
+# Destroy the remaning infrastructure
+echo "--------------------Destroy Remaining Infrastructure--------------------"
+terraform destroy -auto-approve
